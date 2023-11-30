bc65df6618f6e8a8ec06309482a5169222de96b6
LilitVardanyan1992 <lvardanyan890@gmail.com>
2023-11-30 00:57:26 -0800

    initialized sequelize and swagger, created configuration for connection

--- backend/src/app/app.module.ts
+++ backend/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config/dist';
+import { ConfigModule, ConfigService } from '@nestjs/config/dist';
 import { UserModule } from '../user/user.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        models: [],
+      }),
+    }),
+
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
-
-// https://www.youtube.com/watch?v=3gcGfJ4hUBQ&list=PLco1IpJXladbmW9c5Ge1w6oXH2XBaE4XO&index=4
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = app.get(ConfigService);
-  const port = config.get<number>('API_PORT');
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('API_PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('lesson')
+    .setDescription('The test API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port || 3000, () => {
     console.log(`App started on port: ${port}`);
   });
